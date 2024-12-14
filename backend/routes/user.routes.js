8d09f9e8f69ae10dde8c47ae62cc5e605b6ba464
Pratik <prateekraiger123@gmail.com>
2024-12-14 15:40:41 +0530

    added user routes

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  followUnfolloUser,
+  getSuggestedUsers,
+  getUserProfile,
+  updateUser,
+} from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
+router.post("/follow/:id", protectRoute, followUnfolloUser);
+router.post("/suggested", protectRoute, updateUser);
+
+export default router;
