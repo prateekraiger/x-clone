db752ff1a10023fa99525ed512f9322621f3084e
Pratik <prateekraiger123@gmail.com>
2024-12-19 17:09:39 +0530

    Refactor Posts component

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,30 +1,84 @@
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton.jsx";
-import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
 
-const Posts = () => {
-  const isLoading = false;
+const Posts = ({ feedType }) => {
+  // Dynamically determine the endpoint based on feedType
+  const getPostEndPoint = () => {
+    switch (feedType) {
+      case "forYou":
+        return "/api/posts/all";
+      case "following":
+        return "/api/posts/following";
+      default:
+        return "/api/posts/all";
+    }
+  };
+
+  const POST_ENDPOINT = getPostEndPoint();
+
+  // Use useQuery to fetch posts
+  const {
+    data: posts,
+    isLoading,
+    isFetching,
+    error,
+    refetch,
+  } = useQuery({
+    queryKey: ["posts", feedType],
+    queryFn: async () => {
+      try {
+        const res = await fetch(POST_ENDPOINT);
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry: false, 
+  });
+
+
+  useEffect(() => {
+    refetch();
+  }, [feedType, refetch]);
 
   return (
     <>
-      {isLoading && (
+      {/* Display skeleton loader while loading */}
+      {(isLoading || isFetching) && (
         <div className="flex flex-col justify-center">
           <PostSkeleton />
           <PostSkeleton />
           <PostSkeleton />
         </div>
       )}
-      {!isLoading && POSTS?.length === 0 && (
+      {/* Display message if no posts */}
+      {!isLoading && !isFetching && posts?.length === 0 && (
         <p className="text-center my-4">No posts in this tab. Switch 👻</p>
       )}
-      {!isLoading && POSTS && (
+      {/* Display posts */}
+      {!isLoading && posts && (
         <div>
-          {POSTS.map((post) => (
+          {posts.map((post) => (
             <Post key={post._id} post={post} />
           ))}
+        </div>
+      )}
+      {/* Handle API errors */}
+      {error && (
+        <div className="text-center text-red-600 my-4">
+          <p>Error loading posts: {error.message}</p>
         </div>
       )}
     </>
   );
 };
+
 export default Posts;
