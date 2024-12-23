9157e920bb732c3bac7e549ef2153641ff9753e5
Pratik <prateekraiger123@gmail.com>
2024-12-23 15:04:08 +0530

    added like , comment and delete post functinality

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -25,7 +25,7 @@ export const createPost = async (req,res) => {
 
         await newPost.save();
         return res.status(201).json(newPost);
-        
+
     } catch (err) {
         console.log("Error in createPost controller", err.message);
         res.status(500).json({error: "Internal Server Error"});
@@ -38,13 +38,13 @@ export const deletePost = async (req,res) => {
         const currPost = await Post.findById(id);
         if(!currPost) return res.status(404).json({message: "Post not found."});
         if(currPost.user.toString() !== req.user._id.toString()) return res.status(401).json({message: "You are not authorized to delete this post."});
-        
-        
+
+
         if(currPost.img) {
             const imgId = currPost.img.split("/").pop().split(".")[0];
             await cloudinary.uploader.destroy(imgId);
         }
-        
+
         await Post.findByIdAndDelete(id);
         return res.status(200).json({message: `Post ${id} deleted successfully.`});
     } catch (err) {
@@ -61,12 +61,15 @@ export const likeUnlikePost = async (req,res) => {
 
         const userId = req.user._id;
         const hasLiked = post.likes.includes(userId);
-        if(hasLiked) { //already liked then unlike it
+
+        if (hasLiked) { //already liked then unlike it
             await Post.findByIdAndUpdate(postId,{$pull: {likes:userId}});
             await User.findByIdAndUpdate(userId,{$pull: {likedPosts: postId}});
-            const updatedLikes = post.likes.filter((id)=> id.toString()!==userId.toString());
+
+            const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
             res.status(200).json(updatedLikes);
-        } else { //liek the post
+        } else {
+            //like the post
             post.likes.push(userId);
             await post.save();
             await User.findByIdAndUpdate(userId,{$push: {likedPosts: postId}});
@@ -79,7 +82,7 @@ export const likeUnlikePost = async (req,res) => {
                 })
                 await newNotification.save();
             }
-            
+
             res.status(200).json(post.likes);
         }
 
@@ -96,7 +99,7 @@ export const commentOnPost = async (req,res) => {
         const postId = req.params.id;
         const userId = req.user._id;
 
-        if(!text) return res.status(400).json({message: "Comment cannot be empty."}); 
+        if(!text) return res.status(400).json({message: "Comment cannot be empty."});
         const post = await Post.findById(postId);
         if(!post) return res.status(404).json({message: "Post does not exist."});
 
@@ -174,9 +177,9 @@ export const getFollowingPosts = async (req,res) => {
 export const getUserPosts = async (req,res) => {
     try {
         const {username} = req.params;
-        
+
         const user = await User.findOne({username});
-        
+
         if(!user) return res.status(404).json({message: "User not found."});
         const userPosts = await Post.find({user: user._id}).sort({createdAt: -1}).populate({ // the createdAt gives us the latest timesatap one at top, i.e latest post at top in order
             path: "user",
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,42 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
-import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
-import NotificationPage from "./pages/notification/NotificationPage";
-import ProfilePage from "./pages/profile/ProfilePage";
-
+import LoginPage from "./pages/auth/login/LoginPage";
 import Sidebar from "./components/common/Sidebar";
 import RightPanel from "./components/common/RightPanel";
-
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 import { Toaster } from "react-hot-toast";
-import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
   const { data: authUser, isLoading } = useQuery({
-    // we use queryKey to give a unique name to our query and refer to it later
     queryKey: ["authUser"],
     queryFn: async () => {
       try {
         const res = await fetch("/api/auth/me");
         const data = await res.json();
         if (data.error) return null;
-        if (!res.ok) {
-          throw new Error(data.error || "Something went wrong");
-        }
-        // console.log("authUser is here:", data);
+        if (!res.ok) throw new Error(data.error || "Something went wrong!");
+        // console.log("AuthUser is here:", data);
         return data;
       } catch (error) {
         throw new Error(error);
       }
     },
-    retry: false,
   });
 
   if (isLoading) {
     return (
       <div className="h-screen flex justify-center items-center">
-        <LoadingSpinner size="lg" />
+        <LoadingSpinner size="lg"></LoadingSpinner>
       </div>
     );
   }
-
   return (
     <div className="flex max-w-6xl mx-auto">
-      {/* Common component, bc it's not wrapped with Routes */}
+      {/* Common component as it is placed outside the routes */}
       {authUser && <Sidebar />}
       <Routes>
         <Route
@@ -52,12 +44,12 @@ function App() {
           element={authUser ? <HomePage /> : <Navigate to="/login" />}
         />
         <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
-        <Route
           path="/signup"
           element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
         />
         <Route
           path="/notifications"
@@ -69,7 +61,7 @@ function App() {
         />
       </Routes>
       {authUser && <RightPanel />}
-      <Toaster />
+      <Toaster></Toaster>
     </div>
   );
 }
