4575892d12405c4de9efba2a58805fb52373fe67
Pratik <167973139+prateekraiger@users.noreply.github.com>
2025-01-01 21:24:33 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,6 @@ function App() {
         const data = await res.json();
         if (data.error) return null;
         if (!res.ok) throw new Error(data.error || "Something went wrong!");
-        // console.log("AuthUser is here:", data);
         return data;
       } catch (error) {
         throw new Error(error);
@@ -29,39 +28,48 @@ function App() {
 
   if (isLoading) {
     return (
-      <div className="h-screen flex justify-center items-center">
-        <LoadingSpinner size="lg"></LoadingSpinner>
+      <div className="h-screen flex justify-center items-center bg-black">
+        <LoadingSpinner size="lg" />
       </div>
     );
   }
+
   return (
-    <div className="flex max-w-6xl mx-auto">
-      {/* Common component as it is placed outside the routes */}
-      {authUser && <Sidebar />}
-      <Routes>
-        <Route
-          path="/"
-          element={authUser ? <HomePage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/notifications"
-          element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/profile/:username"
-          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
-        />
-      </Routes>
-      {authUser && <RightPanel />}
-      <Toaster></Toaster>
+    <div className="relative min-h-screen w-full bg-black">
+      {/* Background Elements */}
+      <div className="fixed inset-0">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
+        <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]" />
+      </div>
+
+      {/* Content */}
+      <div className="relative flex max-w-6xl mx-auto">
+        {authUser && <Sidebar />}
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/notifications"
+            element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/profile/:username"
+            element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+          />
+        </Routes>
+        {authUser && <RightPanel />}
+        <Toaster />
+      </div>
     </div>
   );
 }
